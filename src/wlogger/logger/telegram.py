ccaa7b0b2e14fab3e0be4f38cc3f85e37edea39c
wendland0d <wendland0d@gmail.com>
2024-01-02 12:57:02 +0300

    new: TelegramLogger in-body structure
    - Fixed TelegramLoggerEntity class
    - - New class attributes added
    - - Added @property methods to work with new attributes
    - Fixed TelegramLogger class
    - - Improved Factory pattern (added @classmethod "create" for passing an object of TelegramLoggerEntity class

--- src/wlogger/logger/telegram.py
+++ src/wlogger/logger/telegram.py
@@ -1,19 +1,65 @@
-import urllib3
-
-from baselogger import BaseLogger
+from src.wlogger.logger.baselogger import BaseLogger
 
 
 class TelegramLoggerEntity(BaseLogger):
     def __init__(self, token, **kwargs):
         self.token: str = token
-        self.web_preview: bool = True
-        self.notification: bool = True
-        self.parse_mode: str = 'HTML'
+        self._web_preview: bool = True
+        self._notification: bool = True
+        self._parse_mode: str = 'HTML'
+        self._custom_message: bool = False
+        self._message = '[{0}]\n{1}\n\n{2}'
+        print('TG created')
 
-        if kwargs['disable_web_page_preview']:
-            self.disabled_web_preview = True
-        if kwargs['disable_notification']:
-            self.notification = False
+    @property
+    def message(self) -> str:
+        return self._message
+
+    @property
+    def web_preview(self):
+        return self._web_preview
+
+    @property
+    def notification(self):
+        return self._notification
+
+    @property
+    def parse_mode(self):
+        return self._parse_mode
+
+    @web_preview.setter
+    def web_preview(self, value: bool):
+        if not isinstance(value, bool):
+            # TODO: Заменить на кастомное исключение
+            raise TypeError()
+        self._web_preview = value
+
+    @notification.setter
+    def notification(self, value: bool):
+        if isinstance(value, bool):
+            self._notification = value
+        else:
+            # TODO: Заменить на кастомное исключение
+            raise TypeError()
+
+
+    @parse_mode.setter
+    def parse_mode(self, value: str):
+        if not isinstance(value, str):
+            # TODO: Заменить на кастомное исключение
+            raise TypeError()
+        if value not in ('HTML', 'MarkdownV2'):
+            # TODO: Заменить на кастомное исключение
+            raise ValueError()
+        self._parse_mode = value
+
+    @message.setter
+    def message(self, value):
+        self._custom_message: bool = True
+        self._message = value
+
+    def _send_message(self):
+        pass
 
     def debug(self):
         pass
@@ -32,5 +78,6 @@ class TelegramLoggerEntity(BaseLogger):
 
 
 class TelegramLogger:
-    def __call__(self, *args, **kwargs) -> TelegramLoggerEntity:
-        return TelegramLoggerEntity()
+    @classmethod
+    def create(cls, **kwargs) -> TelegramLoggerEntity:
+        return TelegramLoggerEntity(token=kwargs['token'])
